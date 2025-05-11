ccbfa009e491b7a3e22f1c030906a1adb116dbda
kien <ngkien112@gmail.com>
2025-05-11 11:57:02 +0700

    push again

--- src/main/java/com/example/bookingcinematicket/config/StripeConfig.java
+++ src/main/java/com/example/bookingcinematicket/config/StripeConfig.java
@@ -7,6 +7,6 @@ import com.stripe.Stripe;
 @Configuration
 public class StripeConfig {
     public StripeConfig() {
-        Stripe.apiKey = "sk_test_51RMqmHFL4Ydn4zhi6gLGJyKc0LzqVr0EDIJ5FIsmMAf1voSK0wTkZlWnD5SYZL1wjNBdIKxvCiPCh4JUWczsmk8c00BXqniB9l";
+        Stripe.apiKey = "KEY";
     }
 }
